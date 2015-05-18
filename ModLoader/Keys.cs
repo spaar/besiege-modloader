6c668cbb725c8da41544859e5f41d62225e71217
ZiMMy <zimmy7z@gmail.com>
2015-05-18 21:51:17 +0300

    git client have forgotten to commit changed files again >:(

--- ModLoader/Keys.cs
+++ ModLoader/Keys.cs
@@ -31,16 +31,4 @@ namespace spaar
             }
         }
     }
-
-    internal class Key
-    {
-        public KeyCode Modifier { get; private set; }
-        public KeyCode Trigger { get; private set; }
-
-        public Key(string modifier, string key)
-        {
-            Modifier = (KeyCode)Enum.Parse(typeof(KeyCode), modifier);
-            Trigger = (KeyCode)Enum.Parse(typeof(KeyCode), key);
-        }
-    }
 }
