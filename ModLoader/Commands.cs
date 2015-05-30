b19b2b43b6375ebdd17cc77607b91daf4ca82176
spaar <spaar.modding@gmail.com>
2015-05-30 16:53:59 +0200

    Improve documentation

--- ModLoader/Commands.cs
+++ ModLoader/Commands.cs
@@ -13,6 +13,26 @@ namespace spaar
     /// <returns>Result of the command, will be printed to the console, if not null or empty string</returns>
     public delegate string CommandCallback(string[] args, IDictionary<string, string> namedArgs);
 
+    /// <summary>
+    /// Commands is used for all things regarding commands. Mainly, you can register your own commands and help messages here.
+    /// </summary>
+    /// <remarks>
+    /// Commands can be entered in the console by the user. The command can be entered by itself if it is only registered once,
+    /// if multiple mods registered the same command they are differentiated by entering <![CDATA[<modname>:<command>]]>.
+    /// Commands can take two types of arguments: named arguments and unnamed arguments. Unnamed arguments are simply values entered
+    /// on the console after the command, seperated by spaces. Named arguments follow this format: <c>--name value</c>.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    ///   Commands.RegisterCommand("myCommand", myCallback);
+    /// </code>
+    /// Registers a new command called myCommand. This can then be used like this:
+    /// <code>
+    ///   myCommand arg1 "second unnamed arg" --namedArg1 value1 --namedArg2 "second value"
+    /// </code>
+    /// If called as above, <c>args</c> passed to the callback will contain <c>["arg1", "second unnamed arg"]</c> and namedArgs
+    /// will contain the following mappings: <c>"namedArg1" -> "value1", "namedArg2" -> "second value"</c>.
+    /// </example>
     public static class Commands
     {
         // Simple struct for representing all information needed about a command
@@ -115,7 +135,7 @@ help - Prints this help message";
         /// </summary>
         /// <param name="command">The command to register</param>
         /// <param name="callback">The callback to be called when the command is entered</param>
-        /// <param name="helpText">Help text for this command. Used for <code><![CDATA[help <mod> <command>]]></code></param>
+        /// <param name="helpText">Help text for this command. Used for <c><![CDATA[help <mod> <command>]]></c></param>
         /// <returns>True if registration succeeded, false otherwise</returns>
         public static bool RegisterCommand(string command, CommandCallback callback, string helpText = "")
         {
@@ -159,6 +179,13 @@ help - Prints this help message";
             helpMessages[mod] = message;
         }
 
+        /// <summary>
+        /// Parse the next argument from parts, starting at i. This respects quotes, so that arguments can contain spaces.
+        /// </summary>
+        /// <param name="parts">The whole argument string, split by spaces</param>
+        /// <param name="i">The index where the argument to get starts</param>
+        /// <param name="newI">The first index after the argument</param>
+        /// <returns>The parsed argument</returns>
         private static string GetArgument(string[] parts, int i, out int newI)
         {
             if (parts[i].StartsWith("\""))
