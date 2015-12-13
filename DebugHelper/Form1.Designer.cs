14628e93964981477541344881f683c52a78c330
spaar <spaar.modding@gmail.com>
2015-12-13 14:13:20 +0100

    Add DebugHelper
    The debug helper facilitates reloading a mod at runtime for debug/development purposes.

---
+++ DebugHelper/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace DebugHelper
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.txtBesiegeLocation = new System.Windows.Forms.TextBox();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.btnReload = new System.Windows.Forms.Button();
+      this.txtModLocation = new System.Windows.Forms.TextBox();
+      this.btnBrowseBesiege = new System.Windows.Forms.Button();
+      this.btnBrowseMod = new System.Windows.Forms.Button();
+      this.txtModName = new System.Windows.Forms.TextBox();
+      this.lblBesiegeLocation = new System.Windows.Forms.Label();
+      this.lblModLocation = new System.Windows.Forms.Label();
+      this.lblModName = new System.Windows.Forms.Label();
+      this.btnReset = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // txtBesiegeLocation
+      // 
+      this.txtBesiegeLocation.Location = new System.Drawing.Point(130, 8);
+      this.txtBesiegeLocation.Name = "txtBesiegeLocation";
+      this.txtBesiegeLocation.Size = new System.Drawing.Size(285, 20);
+      this.txtBesiegeLocation.TabIndex = 0;
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(15, 101);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(132, 36);
+      this.btnStart.TabIndex = 1;
+      this.btnStart.Text = "Start";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // btnReload
+      // 
+      this.btnReload.Location = new System.Drawing.Point(153, 101);
+      this.btnReload.Name = "btnReload";
+      this.btnReload.Size = new System.Drawing.Size(132, 36);
+      this.btnReload.TabIndex = 2;
+      this.btnReload.Text = "Reload Mod";
+      this.btnReload.UseVisualStyleBackColor = true;
+      this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+      // 
+      // txtModLocation
+      // 
+      this.txtModLocation.Location = new System.Drawing.Point(130, 34);
+      this.txtModLocation.Name = "txtModLocation";
+      this.txtModLocation.Size = new System.Drawing.Size(285, 20);
+      this.txtModLocation.TabIndex = 3;
+      // 
+      // btnBrowseBesiege
+      // 
+      this.btnBrowseBesiege.Location = new System.Drawing.Point(421, 6);
+      this.btnBrowseBesiege.Name = "btnBrowseBesiege";
+      this.btnBrowseBesiege.Size = new System.Drawing.Size(29, 23);
+      this.btnBrowseBesiege.TabIndex = 4;
+      this.btnBrowseBesiege.Text = "...";
+      this.btnBrowseBesiege.UseVisualStyleBackColor = true;
+      this.btnBrowseBesiege.Click += new System.EventHandler(this.btnBrowseBesiege_Click);
+      // 
+      // btnBrowseMod
+      // 
+      this.btnBrowseMod.Location = new System.Drawing.Point(421, 32);
+      this.btnBrowseMod.Name = "btnBrowseMod";
+      this.btnBrowseMod.Size = new System.Drawing.Size(29, 23);
+      this.btnBrowseMod.TabIndex = 5;
+      this.btnBrowseMod.Text = "...";
+      this.btnBrowseMod.UseVisualStyleBackColor = true;
+      this.btnBrowseMod.Click += new System.EventHandler(this.btnBrowseMod_Click);
+      // 
+      // txtModName
+      // 
+      this.txtModName.Location = new System.Drawing.Point(130, 60);
+      this.txtModName.Name = "txtModName";
+      this.txtModName.Size = new System.Drawing.Size(285, 20);
+      this.txtModName.TabIndex = 6;
+      // 
+      // lblBesiegeLocation
+      // 
+      this.lblBesiegeLocation.AutoSize = true;
+      this.lblBesiegeLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblBesiegeLocation.Location = new System.Drawing.Point(12, 9);
+      this.lblBesiegeLocation.Name = "lblBesiegeLocation";
+      this.lblBesiegeLocation.Size = new System.Drawing.Size(112, 16);
+      this.lblBesiegeLocation.TabIndex = 7;
+      this.lblBesiegeLocation.Text = "Besiege location:";
+      // 
+      // lblModLocation
+      // 
+      this.lblModLocation.AutoSize = true;
+      this.lblModLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblModLocation.Location = new System.Drawing.Point(36, 35);
+      this.lblModLocation.Name = "lblModLocation";
+      this.lblModLocation.Size = new System.Drawing.Size(88, 16);
+      this.lblModLocation.TabIndex = 8;
+      this.lblModLocation.Text = "Mod location:";
+      // 
+      // lblModName
+      // 
+      this.lblModName.AutoSize = true;
+      this.lblModName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.lblModName.Location = new System.Drawing.Point(49, 61);
+      this.lblModName.Name = "lblModName";
+      this.lblModName.Size = new System.Drawing.Size(75, 16);
+      this.lblModName.TabIndex = 9;
+      this.lblModName.Text = "Mod name:";
+      // 
+      // btnReset
+      // 
+      this.btnReset.Location = new System.Drawing.Point(291, 101);
+      this.btnReset.Name = "btnReset";
+      this.btnReset.Size = new System.Drawing.Size(132, 36);
+      this.btnReset.TabIndex = 10;
+      this.btnReset.Text = "Reset";
+      this.btnReset.UseVisualStyleBackColor = true;
+      this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(464, 148);
+      this.Controls.Add(this.btnReset);
+      this.Controls.Add(this.lblModName);
+      this.Controls.Add(this.lblModLocation);
+      this.Controls.Add(this.lblBesiegeLocation);
+      this.Controls.Add(this.txtModName);
+      this.Controls.Add(this.btnBrowseMod);
+      this.Controls.Add(this.btnBrowseBesiege);
+      this.Controls.Add(this.txtModLocation);
+      this.Controls.Add(this.btnReload);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.txtBesiegeLocation);
+      this.Name = "Form1";
+      this.Text = "Besiege Modding Debug Helper";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtBesiegeLocation;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnReload;
+    private System.Windows.Forms.TextBox txtModLocation;
+    private System.Windows.Forms.Button btnBrowseBesiege;
+    private System.Windows.Forms.Button btnBrowseMod;
+    private System.Windows.Forms.TextBox txtModName;
+    private System.Windows.Forms.Label lblBesiegeLocation;
+    private System.Windows.Forms.Label lblModLocation;
+    private System.Windows.Forms.Label lblModName;
+    private System.Windows.Forms.Button btnReset;
+  }
+}
+
---
+++ DebugHelper/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using Mono.Cecil;
+using SimpleJSON;
+
+namespace DebugHelper
+{
+  public partial class Form1 : Form
+  {
+    private int count = 0;
+
+    private string modsFolder, baseModFileName;
+    
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      modsFolder = (new FileInfo(txtBesiegeLocation.Text).Directory)
+        + "/Besiege_Data/Mods/";
+      baseModFileName = new FileInfo(txtModLocation.Text).Name.Replace(".dll", "");
+
+      if (File.Exists(modsFolder + baseModFileName + ".dll"))
+        File.Delete(modsFolder + baseModFileName + ".dll");
+
+      File.Copy(txtModLocation.Text, modsFolder + baseModFileName + ".dll");
+
+      Process.Start(txtBesiegeLocation.Text, "-popupwindow -enable-debug-server");
+
+      count = 1;
+    }
+
+    private void btnBrowseBesiege_Click(object sender, EventArgs e)
+    {
+      var dialog = new OpenFileDialog();
+      dialog.Filter = "Besiege|Besiege.exe";
+      dialog.ShowDialog();
+      txtBesiegeLocation.Text = dialog.FileName;
+    }
+
+    private void btnBrowseMod_Click(object sender, EventArgs e)
+    {
+      var dialog = new OpenFileDialog();
+      dialog.Filter = "Mod|*.dll";
+      dialog.ShowDialog();
+      txtModLocation.Text = dialog.FileName;
+    }
+
+    private void btnReset_Click(object sender, EventArgs e)
+    {
+      for (; count > 0; count--)
+      {
+        var path = modsFolder + baseModFileName + "-" + count + ".dll";
+        if (File.Exists(path)) File.Delete(path);
+      }
+      if (File.Exists(modsFolder + baseModFileName + ".dll"))
+        File.Delete(modsFolder + baseModFileName + ".dll");
+
+      var configFile = JSON.Parse(
+        File.ReadAllText(modsFolder + "Config/modLoader.json"));
+
+      for (int i = 0; i < configFile.Count; i++)
+      {
+        var item = configFile[i];
+        if (item["key"].Value.StartsWith("modStatus:" + txtModName.Text))
+        {
+          configFile.Remove(i);
+          i--;
+        }
+      }
+
+      File.WriteAllText(modsFolder + "Config/modLoader.json",
+        configFile.ToJSON(0));
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      var besiegeLocation = Environment.ExpandEnvironmentVariables("%BESIEGE_LOCATION%");
+      if (besiegeLocation != "")
+      {
+        txtBesiegeLocation.Text = besiegeLocation + "Besiege.exe";
+      }
+
+      var args = Environment.GetCommandLineArgs();
+
+      if (args.Length > 1)
+      {
+        txtModLocation.Text = args[1];
+      }
+      if (args.Length > 2)
+      {
+        txtModName.Text = args[2];
+      }
+    }
+
+    private void btnReload_Click(object sender, EventArgs e)
+    {
+      if (count == 0) return;
+
+      var newModPath = modsFolder + baseModFileName + "-" + count + ".dll";
+      File.Copy(txtModLocation.Text, newModPath);
+
+      // Rewrite 'Name' and assembly version of new file
+      var modDefinition = AssemblyDefinition.ReadAssembly(newModPath);
+      var ver = modDefinition.Name.Version;
+      var newVersion = new Version(ver.Major, ver.Minor, ver.Build, count);
+      modDefinition.Name.Version = newVersion;
+      modDefinition.Write(newModPath);
+
+      // Connect to mod loader, disable old mod, load new mod, enable new mod
+      var disableMessage = Encoding.UTF8.GetBytes(
+        "disableMod " +
+        (count == 1 ? txtModName.Text : txtModName.Text + "-" + (count-1)) + "\n");
+      var loadMessage = Encoding.UTF8.GetBytes(
+        "loadMod " + newModPath + " " + txtModName.Text + "-" + count + "\n");
+      var enableMessage = Encoding.UTF8.GetBytes(
+        "enableMod " + txtModName.Text + "-" + count + "\n");
+
+      var client = new TcpClient();
+      client.Connect("localhost", 5000);
+      client.GetStream().Write(disableMessage, 0, disableMessage.Length);
+      Thread.Sleep(200);
+      client.GetStream().Write(loadMessage, 0, loadMessage.Length);
+      Thread.Sleep(200);
+      client.GetStream().Write(enableMessage, 0, enableMessage.Length);
+      Thread.Sleep(200);
+      client.Close();
+
+      count++;
+    }
+  }
+}
