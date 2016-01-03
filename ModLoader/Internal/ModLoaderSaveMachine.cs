7e268ef3767d21387cb1e4e814d71c39f82c5ba5
spaar <spaar.modding@gmail.com>
2016-01-03 17:24:45 +0100

    Fix load callback being called before save callback when saving machines

--- ModLoader/Internal/ModLoaderSaveMachine.cs
+++ ModLoader/Internal/ModLoaderSaveMachine.cs
@@ -6,6 +6,7 @@ namespace spaar.ModLoader.Internal
 {
   public class ModLoaderSaveMachine : MySaveMachine
   {
+    private bool saving = false;
 
     public void CopyFrom(MySaveMachine o)
     {
@@ -26,6 +27,8 @@ namespace spaar.ModLoader.Internal
 
     public override string WriteBooleanArray(bool[] arrayToUse)
     {
+      saving = true;
+
       // Abuse this method to add data to the end, simplest way to hook into saving
       string result = base.WriteBooleanArray(arrayToUse);
 
@@ -40,10 +43,37 @@ namespace spaar.ModLoader.Internal
     {
       base.LoadAll(namey);
 
+      saving = false;
+      StartCoroutine(DelayedLoading(namey, false));
+    }
+
+    public override void WorkshopLoadAll(string fullPath)
+    {
+      base.WorkshopLoadAll(fullPath);
+
+      saving = false;
+      StartCoroutine(DelayedLoading(fullPath, true));
+    }
+
+    private System.Collections.IEnumerator DelayedLoading(string namey,
+      bool fullPath)
+    {
+      // If the machine is saved, LoadAll will nevertheless be called before
+      // WriteBooleanArray will be. Because of this, when loading, we wait for
+      // 4 frames (after which WriteBooleanArray should have been called) and
+      // then check whether we're actually saving and not loading in order to
+      // avoid false calls to the load callbacks.
+      for (int i = 0; i < 4; i++)
+      {
+        yield return null;
+      }
+
+      if (saving == true) yield break;
+
       try
       {
-        var streamReader = new StreamReader(Application.dataPath
-          + "/SavedMachines/" + namey + ".bsg");
+        var streamReader = new StreamReader(fullPath ?
+          namey : Application.dataPath + "/SavedMachines/" + namey + ".bsg");
         var saveData = streamReader.ReadToEnd();
         streamReader.Close();
 
@@ -51,19 +81,8 @@ namespace spaar.ModLoader.Internal
       }
       catch (Exception)
       {
-
+        Debug.LogError("[MachineData] Error while calling load callbacks.");
       }
-    }
-
-    public override void WorkshopLoadAll(string fullPath)
-    {
-      base.WorkshopLoadAll(fullPath);
-
-      var streamReader = new StreamReader(fullPath);
-      var saveData = streamReader.ReadToEnd();
-      streamReader.Close();
-
-      MachineData.LoadData(saveData);
     }
 
   }
