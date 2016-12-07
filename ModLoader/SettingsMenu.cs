c3b437a9890864e57ae3266c970f0df37b43675e
Lenart Bezek <lench4991@gmail.com>
2016-12-07 23:54:57 +0100

    Enable mods to keep reference to SettingButton after creating it.

--- ModLoader/SettingsMenu.cs
+++ ModLoader/SettingsMenu.cs
@@ -5,187 +5,224 @@ using UnityEngine.SceneManagement;
 
 namespace spaar.ModLoader
 {
-  /// <summary>
-  /// Callback delegate for a toggle setting.
-  /// </summary>
-  /// <param name="active">Whether the toggle is active</param>
-  public delegate void SettingsToggle(bool active);
-
-  /// <summary>
-  /// SettingsMenu contains methods for adding a setting (toggle button) to the
-  /// settings drop-down of Besiege.
-  /// </summary>
-  public class SettingsMenu : SingleInstance<SettingsMenu>
-  {
-    public override string Name { get { return "spaar's Mod Loader: Settings Utility"; } }
-
-    private struct SettingsButton
-    {
-      public string text;
-      public SettingsToggle cb;
-      public bool defaultValue;
-      public int fontSize;
-    }
-
-    private static int numRegistered = 0;
-
-    private static List<SettingsButton> toAdd = new List<SettingsButton>();
+    /// <summary>
+    /// Callback delegate for a toggle setting.
+    /// </summary>
+    /// <param name="active">Whether the toggle is active</param>
+    public delegate void SettingsToggle(bool active);
 
     /// <summary>
-    /// Registers a new toggle button. It will be placed below all others
-    /// that are currently registered.
+    /// 
     /// </summary>
-    /// <param name="text">The text to display on the button</param>
-    /// <param name="cb">Callback to call when the button is clicked</param>
-    /// <param name="defaultValue">Starting state of the toggle</param>
-    /// <param name="fontSize">Font size of the text on the button</param>
-    public static void RegisterSettingsButton(string text, SettingsToggle cb,
-      bool defaultValue = false, int fontSize = 0)
+    public class SettingsButton
     {
-      var button = new SettingsButton()
-      {
-        text = text,
-        cb = cb,
-        defaultValue = defaultValue,
-        fontSize = fontSize
-      };
-      toAdd.Add(button);
+        public string Text;
 
-      if (Game.AddPiece != null)
-        RegisterSettingsButtonInternal(button);
-    }
-
-    private void Start()
-    {
-      Internal.ModLoader.MakeModule(this);
-
-      SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-      if (Game.AddPiece != null)
-      {
-        numRegistered = 0;
-        foreach (var button in toAdd)
+        public bool Value
         {
-          RegisterSettingsButtonInternal(button);
-        }
-      }
-    }
-
-    private static Transform modSection;
-
-    private static void RegisterSettingsButtonInternal(SettingsButton button)
-    {
-      var settingsObjects = GameObject.Find("Settings").transform
-        .FindChild("SettingsObjects");
-      var bottomDefaultSetting = settingsObjects.FindChild("GOD/INFINITE AMMO");
-      Vector3 SettingSize = new Vector3(0.748f, 0.375f);
-
-      if (modSection == null)
-      {
-        // Create a MODS section
-        var settings = settingsObjects.FindChild("SETTINGS");
-
-        var modsPos = settings.position;
-        modsPos.y = bottomDefaultSetting.position.y - 1.2f;
-        modsPos.z = 7.0f; // Prevent blocks GUI from being over the toggles
-
-        modSection = (Transform)Instantiate(settings, modsPos,
-          settings.rotation);
-        modSection.parent = settingsObjects;
-        modSection.name = "MOD SETTINGS";
-
-        foreach (Transform child in modSection)
-        {
-          if (child.name == "GENERAL")
-          {
-            child.GetComponent<TextMesh>().text = "M O D S";
-            child.name = "Title";
-          }
-          else
-          {
-            Destroy(child.gameObject);
-          }
+            get { return _value; }
+            set
+            {
+                settingsComponent?.SetOn(value);
+                OnToggle?.Invoke(value);
+                _value = value;
+            }
         }
 
-        // Adjust background to include mods section title
-        var bg = settingsObjects.FindChild("BG");
-        var bgScale = bg.localScale;
-        bgScale.y += 2.85f;
-        bg.localScale = bgScale;
-        // Also need to remove background of infinite ammo as long as it's
-        // the only thing in its row.
-        Destroy(settingsObjects.FindChild("GOD/INFINITE AMMO/BG (1)").gameObject);
+        public int FontSize;
 
-        bg.gameObject.AddComponent<BoxCollider>();
-        var scrollCollider = new GameObject("Scrolling").transform;
-        scrollCollider.parent = settingsObjects;
-        scrollCollider.rotation = bg.rotation;
-        scrollCollider.localScale = bg.localScale;
-        scrollCollider.gameObject.layer = bg.gameObject.layer;
-        var pos = bg.position;
-        pos.z = 15.0f; // Put collider behind all settings items
-        scrollCollider.position = pos;
-        scrollCollider.gameObject.AddComponent<ScrollSettingsMenu>()
-          .settingsObjects = settingsObjects;
-      }
-      var settingPos = bottomDefaultSetting.position;
+        /// <summary>
+        /// Called on Value change.
+        /// </summary>
+        public SettingsToggle OnToggle;
 
-      settingPos.x += (numRegistered % 2) * SettingSize.x;
-      settingPos.y -= 1.25f + (numRegistered / 2) * SettingSize.y;
-      settingPos.z = 7.0f; // Prevent blocks GUI from being over any toggles
+        private bool _value;
+        internal SettingsComponent settingsComponent;
 
-      var fxaa = settingsObjects.FindChild("SETTINGS/FXAA");
-
-      var newSetting = (Transform)Instantiate(fxaa, settingPos, fxaa.rotation);
-      newSetting.parent = modSection;
-
-      if (numRegistered % 2 == 0)
-      {
-        // Expand background to include new toggle
-        var background = settingsObjects.FindChild("BG");
-        var backgroundScale = background.localScale;
-        backgroundScale.y += SettingSize.y * 2;
-        background.localScale = backgroundScale;
-
-        // Expand the scrolling object to the same size
-        var scrolling = settingsObjects.FindChild("Scrolling");
-        scrolling.localScale = backgroundScale;
-        scrolling.GetComponent<ScrollSettingsMenu>().CalcBounds();
-
-        // Check whether the new element row is outside of the screen
-        // and enable scrolling if it is
-        var lowestPoint = newSetting.position - SettingSize;
-        var cam = GameObject.Find("HUD Cam").GetComponent<Camera>();
-        if (cam.WorldToViewportPoint(lowestPoint).y < 0.0f)
+        public void Destroy()
         {
-          scrolling.GetComponent<ScrollSettingsMenu>().scrollingEnabled = true;
+            Object.Destroy(settingsComponent.gameObject);
         }
-      }
-
-      var newSettingButton = newSetting.FindChild("AA BUTTON");
-      var newSettingText = newSetting.FindChild("AA text");
-      newSetting.gameObject.name = button.text;
-      newSettingButton.gameObject.name = button.text + " button";
-      newSettingText.gameObject.name = button.text + " text";
-      var textMesh = newSettingText.gameObject.GetComponent<TextMesh>();
-      textMesh.text = button.text;
-      textMesh.fontSize = button.fontSize;
-
-      var settingsComponent
-        = newSettingButton.gameObject.AddComponent<SettingsComponent>();
-
-      var fxaaToggle = newSettingButton.gameObject.GetComponent<ToggleAA>();
-      settingsComponent.darkMaterial = fxaaToggle.darkMaterial;
-      settingsComponent.redMaterial = fxaaToggle.redMaterial;
-      Destroy(fxaaToggle);
-
-      settingsComponent.SetCallback(button.cb);
-      settingsComponent.SetOn(button.defaultValue);
-
-      numRegistered++;
     }
-  }
+
+    /// <summary>
+    /// SettingsMenu contains methods for adding a setting (toggle button) to the
+    /// settings drop-down of Besiege.
+    /// </summary>
+    public class SettingsMenu : SingleInstance<SettingsMenu>
+    {
+        public override string Name { get { return "spaar's Mod Loader: Settings Utility"; } }
+
+        private static int numRegistered = 0;
+
+        private static List<SettingsButton> toAdd = new List<SettingsButton>();
+
+        /// <summary>
+        /// Registers a new toggle button. It will be placed below all others
+        /// that are currently registered.
+        /// </summary>
+        /// <param name="text">The text to display on the button</param>
+        /// <param name="cb">Callback to call when the button is clicked</param>
+        /// <param name="defaultValue">Starting state of the toggle</param>
+        /// <param name="fontSize">Font size of the text on the button</param>
+        public static void RegisterSettingsButton(string text, SettingsToggle cb,
+          bool defaultValue = false, int fontSize = 0)
+        {
+            var button = new SettingsButton()
+            {
+                Text = text,
+                OnToggle = cb,
+                Value = defaultValue,
+                FontSize = fontSize
+            };
+            toAdd.Add(button);
+
+            if (Game.AddPiece != null)
+                RegisterSettingsButtonInternal(button);
+        }
+
+        public static void RegisterSettingsButton(SettingsButton button)
+        {
+            toAdd.Add(button);
+
+            if (Game.AddPiece != null)
+                RegisterSettingsButtonInternal(button);
+        }
+
+        private void Start()
+        {
+            Internal.ModLoader.MakeModule(this);
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (Game.AddPiece != null)
+            {
+                numRegistered = 0;
+                foreach (var button in toAdd)
+                {
+                    RegisterSettingsButtonInternal(button);
+                }
+            }
+        }
+
+        private static Transform modSection;
+
+        private static void RegisterSettingsButtonInternal(SettingsButton button)
+        {
+            var settingsObjects = GameObject.Find("Settings").transform
+              .FindChild("SettingsObjects");
+            var bottomDefaultSetting = settingsObjects.FindChild("GOD/INFINITE AMMO");
+            Vector3 SettingSize = new Vector3(0.748f, 0.375f);
+
+            if (modSection == null)
+            {
+                // Create a MODS section
+                var settings = settingsObjects.FindChild("SETTINGS");
+
+                var modsPos = settings.position;
+                modsPos.y = bottomDefaultSetting.position.y - 1.2f;
+                modsPos.z = 7.0f; // Prevent blocks GUI from being over the toggles
+
+                modSection = (Transform)Instantiate(settings, modsPos,
+                  settings.rotation);
+                modSection.parent = settingsObjects;
+                modSection.name = "MOD SETTINGS";
+
+                foreach (Transform child in modSection)
+                {
+                    if (child.name == "GENERAL")
+                    {
+                        child.GetComponent<TextMesh>().text = "M O D S";
+                        child.name = "Title";
+                    }
+                    else
+                    {
+                        Destroy(child.gameObject);
+                    }
+                }
+
+                // Adjust background to include mods section title
+                var bg = settingsObjects.FindChild("BG");
+                var bgScale = bg.localScale;
+                bgScale.y += 2.85f;
+                bg.localScale = bgScale;
+                // Also need to remove background of infinite ammo as long as it's
+                // the only thing in its row.
+                Destroy(settingsObjects.FindChild("GOD/INFINITE AMMO/BG (1)").gameObject);
+
+                bg.gameObject.AddComponent<BoxCollider>();
+                var scrollCollider = new GameObject("Scrolling").transform;
+                scrollCollider.parent = settingsObjects;
+                scrollCollider.rotation = bg.rotation;
+                scrollCollider.localScale = bg.localScale;
+                scrollCollider.gameObject.layer = bg.gameObject.layer;
+                var pos = bg.position;
+                pos.z = 15.0f; // Put collider behind all settings items
+                scrollCollider.position = pos;
+                scrollCollider.gameObject.AddComponent<ScrollSettingsMenu>()
+                  .settingsObjects = settingsObjects;
+            }
+            var settingPos = bottomDefaultSetting.position;
+
+            settingPos.x += (numRegistered % 2) * SettingSize.x;
+            settingPos.y -= 1.25f + (numRegistered / 2) * SettingSize.y;
+            settingPos.z = 7.0f; // Prevent blocks GUI from being over any toggles
+
+            var fxaa = settingsObjects.FindChild("SETTINGS/FXAA");
+
+            var newSetting = (Transform)Instantiate(fxaa, settingPos, fxaa.rotation);
+            newSetting.parent = modSection;
+
+            if (numRegistered % 2 == 0)
+            {
+                // Expand background to include new toggle
+                var background = settingsObjects.FindChild("BG");
+                var backgroundScale = background.localScale;
+                backgroundScale.y += SettingSize.y * 2;
+                background.localScale = backgroundScale;
+
+                // Expand the scrolling object to the same size
+                var scrolling = settingsObjects.FindChild("Scrolling");
+                scrolling.localScale = backgroundScale;
+                scrolling.GetComponent<ScrollSettingsMenu>().CalcBounds();
+
+                // Check whether the new element row is outside of the screen
+                // and enable scrolling if it is
+                var lowestPoint = newSetting.position - SettingSize;
+                var cam = GameObject.Find("HUD Cam").GetComponent<Camera>();
+                if (cam.WorldToViewportPoint(lowestPoint).y < 0.0f)
+                {
+                    scrolling.GetComponent<ScrollSettingsMenu>().scrollingEnabled = true;
+                }
+            }
+
+            var newSettingButton = newSetting.FindChild("AA BUTTON");
+            var newSettingText = newSetting.FindChild("AA text");
+            newSetting.gameObject.name = button.Text;
+            newSettingButton.gameObject.name = button.Text + " button";
+            newSettingText.gameObject.name = button.Text + " text";
+            var textMesh = newSettingText.gameObject.GetComponent<TextMesh>();
+            textMesh.text = button.Text;
+            textMesh.fontSize = button.FontSize;
+
+            var settingsComponent
+              = newSettingButton.gameObject.AddComponent<SettingsComponent>();
+
+            var fxaaToggle = newSettingButton.gameObject.GetComponent<ToggleAA>();
+            settingsComponent.darkMaterial = fxaaToggle.darkMaterial;
+            settingsComponent.redMaterial = fxaaToggle.redMaterial;
+            Destroy(fxaaToggle);
+
+            settingsComponent.SetCallback(button.OnToggle);
+            settingsComponent.SetOn(button.Value);
+
+            // Pass settings component reference to button.
+            button.settingsComponent = settingsComponent;
+
+            numRegistered++;
+        }
+    }
 }
