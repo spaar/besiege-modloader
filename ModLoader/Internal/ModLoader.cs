6b12727b4f27433bea227e6b871b1b78b6a853a2
spaar <spaar.modding@gmail.com>
2017-06-06 23:27:00 +0200

    Update for Besiege v0.45a

--- ModLoader/Internal/ModLoader.cs
+++ ModLoader/Internal/ModLoader.cs
@@ -30,7 +30,7 @@ namespace spaar.ModLoader.Internal
     /// <summary>
     /// The currently running Besiege version.
     /// </summary>
-    public static readonly string BesiegeVersion = "v0.45";
+    public static readonly string BesiegeVersion = "v0.45a";
     public static readonly Version ModLoaderVersion = new Version(1, 7, 0);
 
     public override string Name { get { return "spaar's Mod Loader"; } }
